50673127a3cf34770ba240c3e9f4555667302572
Sebastián Castillo <tantrum5535@gmail.com>
2025-03-30 17:15:44 -0300

    update build script to run tests before building; add failing test case for root endpoint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,6 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
+      expect(true).toBe(false);
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
