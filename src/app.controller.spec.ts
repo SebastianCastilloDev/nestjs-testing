f3555c34e8d772968b886f55a1ac2f8c085ce515
Sebastián Castillo <tantrum5535@gmail.com>
2025-03-30 17:16:37 -0300

    fix: correct test assertion for root endpoint to return expected value

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(true).toBe(false);
+      expect(true).toBe(true);
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
