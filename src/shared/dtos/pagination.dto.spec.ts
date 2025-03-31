b26f91b217fb069b10fd22b37d8e7f6e8aeeb561
Sebastián Castillo <tantrum5535@gmail.com>
2025-03-31 15:02:28 -0300

    feat: update reflect-metadata to version 0.2.2 and add tests for PaginationDto validation

---
+++ src/shared/dtos/pagination.dto.spec.ts
@@ -0,0 +1,52 @@
+import 'reflect-metadata';
+
+import { validate } from 'class-validator';
+import { PaginationDto } from './pagination.dto';
+import { error } from 'console';
+
+describe('PaginationDto', () => {
+  it('should validate with default values', async () => {
+    const dto = new PaginationDto();
+
+    const errors = await validate(dto);
+    console.log(errors);
+
+    expect(errors.length).toBe(0);
+  });
+
+  it('should validate with valid data', async () => {
+    const dto = new PaginationDto();
+    dto.limit = 20;
+    dto.page = 10;
+    const errors = await validate(dto);
+    expect(errors.length).toBe(0);
+  });
+
+  it('should validate with invalid page', async () => {
+    const dto = new PaginationDto();
+    dto.page = 0;
+    const errors = await validate(dto);
+
+    expect(errors.length).toBeGreaterThanOrEqual(1);
+
+    errors.forEach((error) => {
+      if (error.property === 'page') {
+        expect(error.constraints?.min).toBeDefined();
+      }
+    });
+  });
+
+  it('should validate with invalid limit', async () => {
+    const dto = new PaginationDto();
+    dto.limit = -1;
+    const errors = await validate(dto);
+
+    expect(errors.length).toBeGreaterThanOrEqual(1);
+
+    errors.forEach((error) => {
+      if (error.property === 'limit') {
+        expect(error.constraints?.min).toBeDefined();
+      }
+    });
+  });
+});
